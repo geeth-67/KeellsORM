c16fe2931bddf6083187a28f89b85b02beacd86e
vishwageethanjana <geeth.me67@gmail.com>
2026-02-22 20:09:14 +0530

    add database config

--- database_config.py
+++ database_config.py
@@ -12,3 +12,10 @@ engine = create_async_engine(
     future=True,
 )
 
+async_session = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autocommit=False,
+)
+
